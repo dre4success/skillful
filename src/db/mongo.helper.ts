d909badf82af68beb75b62fca46db2131b072ede
dre4success <dre4success@live.com>
2020-06-22 20:06:57 +0100

    mongo db connection helper

---
+++ src/db/mongo.helper.ts
@@ -0,0 +1,32 @@
+// import * as mongo from 'mongodb'
+import { MongoClient, Collection } from 'mongodb';
+import * as dotenv from 'dotenv';
+dotenv.config({ path: 'application.env' });
+
+export class MongoHelper {
+  public static client: MongoClient;
+  constructor() {}
+
+  /**
+   * connect
+   */
+  public static async connect(url: string) {
+    const client: MongoClient = new MongoClient(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    MongoHelper.client = client;
+    return await client.connect();
+  }
+
+  public static table(collection: string): Collection<any> {
+    if (!MongoHelper.client) {
+      MongoHelper.connect(`${process.env.MONGO_URI_STAGING}`)
+        .then((client) => (MongoHelper.client = client))
+        .catch((error) => {
+          console.log('error from collection table', (error && error.message) || error);
+        });
+    }
+    return MongoHelper.client.db('iamskillful').collection(collection);
+  }
+}
