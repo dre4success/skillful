5d30d21d3581b48cb7a63a2cad179a432e0178ad
dre4success <dre4success@live.com>
2020-08-23 21:26:39 +0100

    update deployment script

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
-STACK_NAME=awsbootstrap
+STACK_NAME=iamskillful
 REGION=eu-west-1
 CLI_PROFILE=dre4success
 
 EC2_INSTANCE_TYPE=t3a.micro
+
+AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile dre4success \
+  --query "Account" --output text`
+CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID"
+
+CFN_BUCKET="$STACK_NAME-cfn-$AWS_ACCOUNT_ID"
+# Generate a personal access token with repo and admin:repo_hook
+# permissions from https://github.com/settings/tokens
+GH_ACCCESS_TOKEN=$(cat ~/.iamskillful/token)
+GH_OWNER=$(cat ~/.iamskillful/owner)
+GH_REPO=$(cat ~/.iamskillful/repo)
+GH_BRANCH=master
 
 # Deploys static resources
 echo -e "\n\n========= Deploying setup.yml ======="
@@ -12,11 +24,29 @@ aws cloudformation deploy \
   --region $REGION \
   --profile $CLI_PROFILE \
   --stack-name $STACK_NAME-setup \
-  --template-file setup..yml \
+  --template-file setup.yml \
   --no-fail-on-empty-changeset \
   --capabilities CAPABILITY_NAMED_IAM \
   --parameter-overrides \
-    CodePipelineBucket=$CODEPIPELINE_BUCKET
+    CodePipelineBucket=$CODEPIPELINE_BUCKET \
+    CloudFormationBucket=$CFN_BUCKET
+
+# Package up CloudFormation templates into an S3 bucket
+echo -e "\n\n========= Packaging main.yml ========"
+mkdir -p ./cfn_output
+
+PACKAGE_ERR="$(aws cloudformation package \
+  --region $REGION \
+  --profile $CLI_PROFILE \
+  --template main.yml \
+  --s3-bucket $CFN_BUCKET \
+  --output-template-file ./cfn_output/main.yml 2>&1)"
+
+if ! [[ $PACKAGE_ERR =~ "Successfully packaged artifacts" ]]; then
+  echo "ERROR while running 'aws cloudformation package' command:"
+  echo $PACKAGE_ERR
+  exit 1
+fi
 
 # Deploy the CloudFormation template
 echo -e "\n\n========= Deploying main.yml ========"
@@ -24,19 +54,19 @@ aws cloudformation deploy \
   --region $REGION \
   --profile $CLI_PROFILE \
   --stack-name $STACK_NAME \
-  --template-file main.yml \
+  --template-file ./cfn_output/main.yml \
   --no-fail-on-empty-changeset \
   --capabilities CAPABILITY_NAMED_IAM \
   --parameter-overrides \
-    EC2InstanceType=$EC2_INSTANCE_TYPE
+    EC2InstanceType=$EC2_INSTANCE_TYPE \
+    GitHubOwner=$GH_OWNER \
+    GitHubRepo=$GH_REPO \
+    GitHubBranch=$GH_BRANCH \
+    GitHubPersonalAccessToken=$GH_ACCCESS_TOKEN \
+    CodePipelineBucket=$CODEPIPELINE_BUCKET
 
 # If the deploy succeeded, show the DNS name of the created instance
 if [ $? -eq 0 ]; then
 	aws cloudformation list-exports \
 		--profile dre4success
-		--query "Exports[?Name=='InstanceEndpoint'].Value"
 fi
-
-AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile dre4success \
-  --query "Account" --output text`
-CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID"
