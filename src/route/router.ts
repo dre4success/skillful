42d7231eae7217e761001be306dc069051b35250
dre4success <dre4success@live.com>
2020-09-13 10:41:21 +0100

    load new routes

--- src/route/router.ts
+++ src/route/router.ts
@@ -3,12 +3,15 @@ const router = Router();
 import User from '../controllers/user';
 import Upload from '../controllers/upload';
 import { asyncError } from '../errorhandler/errorhandler';
+import { auth } from '../middleware/auth';
 
 let userController = new User();
 let uploadController = new Upload();
 
 router.route('/signup').post(asyncError(userController.signup));
 router.route('/login').post(asyncError(userController.login));
+router.route('/user').get(auth, asyncError(userController.getALoggedInUser));
+router.route('/user').put(auth, asyncError(userController.editProfile));
 
 router.route('/upload/profile').post(asyncError(uploadController.uploadProfileImage));
 
